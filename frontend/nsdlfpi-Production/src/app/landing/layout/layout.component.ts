6c33a77f77de4ce8f40aad3ddecbba3f31da91d2
Vaibhav Malave <vaibhav.malave@oneture.com>
2025-04-02 15:00:28 +0530

    fix: added publish

--- frontend/nsdlfpi-Production/src/app/landing/layout/layout.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-layout',
-  standalone: false,
-  templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss',
-})
-export class LayoutComponent {}
