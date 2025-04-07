fa95a25f7aa6b5458fe4fb9b3f57cbf08316c064
Vaibhav Malave <vaibhav.malave@oneture.com>
2025-04-07 11:23:36 +0530

    fix: added scripts

---
+++ database/08_BeneficialOwners_alter.sql
@@ -0,0 +1,6 @@
+ALTER TABLE nsdlcaf.dbo.draft_BeneficialOwners ALTER COLUMN PercentageStakeHeld varchar(255) NULL;
+ALTER TABLE nsdlcaf.dbo.audit_BeneficialOwners ALTER COLUMN PercentageStakeHeld varchar(255) NULL;
+ALTER TABLE nsdlcaf.dbo.BeneficialOwners ALTER COLUMN PercentageStakeHeld varchar(255) NULL;
+ALTER TABLE nsdlcaf.dbo.MaterialShareholder ALTER COLUMN PercentageStakeHeld varchar(255) NULL;
+ALTER TABLE nsdlcaf.dbo.audit_MaterialShareholder ALTER COLUMN PercentageStakeHeld varchar(255) NULL;
+ALTER TABLE nsdlcaf.dbo.draft_MaterialShareholder ALTER COLUMN PercentageStakeHeld varchar(255) NULL;
